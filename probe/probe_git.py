1cddcb16e4c9845978bbe2176e671888530ac067
macbook <seaman@macbook>
2023-01-02 02:34:08 -0700

    Fix tests

--- probe/probe_git.py
+++ probe/probe_git.py
@@ -4,7 +4,7 @@ from publish.shell import shell
 
 
 def test_git_files():
-    return "Git " + check_file_count(recursive_files(".git"), 2000, 9000)
+    return "Git " + check_file_count(recursive_files(".git"), 800, 9000)
 
 
 def test_git_status():

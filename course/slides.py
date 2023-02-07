3f6cb2d4d4227243a860dbe1fbd635bf034f8270
Mark Seaman <seaman@SeaMac>
2023-02-07 13:35:16 -0700

    Slide show

--- course/slides.py
+++ course/slides.py
@@ -10,32 +10,32 @@ from publish.files import read_file, read_json, write_file
 from publish.text import text_join, text_lines
 
 
-def build_slides(markdown_path, website_path, course):
-    def build_slide_deck(slides_file, markdown_file, **kwargs):
-        text = read_file(markdown_file)
-        text = render_slides(text, **kwargs)
-        template_name = "course_slides.html"
-        kwargs["text"] = text
-        html = render_to_string(template_name, kwargs)
-        write_file(slides_file, html)
+# def build_slides(markdown_path, website_path, course):
+#     def build_slide_deck(slides_file, markdown_file, **kwargs):
+#         text = read_file(markdown_file)
+#         text = render_slides(text, **kwargs)
+#         template_name = "course_slides.html"
+#         kwargs["text"] = text
+#         html = render_to_string(template_name, kwargs)
+#         write_file(slides_file, html)
 
-    markdown = f"{markdown_path}/lesson"
-    assert exists(markdown)
+#     markdown = f"{markdown_path}/lesson"
+#     assert exists(markdown)
 
-    slides = f"{website_path}/slides"
-    assert exists(slides)
+#     slides = f"{website_path}/slides"
+#     assert exists(slides)
 
-    json = f"Documents/shrinking-world.com/{course}/slides_settings.json"
-    assert exists(json)
-    settings = read_json(json)
-    assert settings
+#     json = f"Documents/shrinking-world.com/{course}/slides_settings.json"
+#     assert exists(json)
+#     settings = read_json(json)
+#     assert settings
 
-    for s in listdir(markdown):
-        lesson = s.replace(".md", "")
-        settings["lesson"] = lesson
-        markdown_file = f"{markdown}/{s}"
-        slides_file = f"{slides}/{lesson}.html"
-        build_slide_deck(slides_file, markdown_file, **settings)
+#     for s in listdir(markdown):
+#         lesson = s.replace(".md", "")
+#         settings["lesson"] = lesson
+#         markdown_file = f"{markdown}/{s}"
+#         slides_file = f"{slides}/{lesson}.html"
+#         build_slide_deck(slides_file, markdown_file, **settings)
 
 
 def render_slides(text, **kwargs):
@@ -62,13 +62,15 @@ def render_slides(text, **kwargs):
         return render_to_string("slides_section.html", kwargs)
 
     return text_join(
-        [render_section(s, kwargs) for s in render_sections(text, **kwargs)][1:]
+        [render_section(s, kwargs)
+         for s in render_sections(text, **kwargs)][1:]
     )
 
 
 def slides_view_context(**kwargs):
     course = get_course(kwargs["course"])
-    lesson = Content.objects.get(course=course, order=kwargs["order"], doctype="lesson")
+    lesson = Content.objects.get(
+        course=course, order=kwargs["order"], doctype="lesson")
     json = f"Documents/shrinking-world.com/{course.name}/slides_settings.json"
     kwargs = read_json(json)
     course = kwargs["course"]

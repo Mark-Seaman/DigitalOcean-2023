b0296d97532358b1680f2076eb2a0a35649549c7
Mark Seaman <seaman@SeaMac>
2023-08-01 11:58:30 -0600

    Fix build with delete

--- publish/management/commands/build.py
+++ publish/management/commands/build.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
             load_data()
         elif cmd == "pubs":
             print("BUILD PUBS")
-            print(build_pubs(verbose=True, verbose=True))
+            print(build_pubs(verbose=True, delete=True))
         elif cmd == "review":
             review_file()
         elif cmd == "user":

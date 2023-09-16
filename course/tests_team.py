4c9009bf3545d54bb7138db5f003e421236121e5
macbook <seaman@macbook>
2023-09-15 19:36:38 -0600

    Todo

--- course/tests_team.py
+++ course/tests_team.py
@@ -60,7 +60,7 @@ class TeamPagesTest(DjangoTest):
         Team.objects.create(name='Development Test', pk=6,
                             github='https://github.com', server='https://digitalocean.com')
         x = get_page('6', '1')
-        self.assertEqual(len(x['html']), 1669)
+        self.assertEqual(len(x['html']), 1771)
         y = 'Development Test - Milestone 1 - Project Charter'
         self.assertEqual(x['title'], y)
         # print(len(x['html']), 'characters')
@@ -70,7 +70,7 @@ class TeamPagesTest(DjangoTest):
         Team.objects.create(name='Development Test', pk=6,
                             github='https://github.com', server='https://digitalocean.com')
         x = get_page('6', '2')
-        self.assertEqual(len(x['html']), 1805)
+        self.assertEqual(len(x['html']), 1870)
         y = 'Development Test - Milestone 2 - Technical Feasibility'
         self.assertEqual(x['title'], y)
         # print(len(x['html']), 'characters')

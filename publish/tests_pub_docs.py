d9cb8c6e14c9dad541b832f3cf6cfa7002be9ecb
Mark Seaman <seaman@SeaMac>
2023-10-05 16:49:08 -0600

    Setup new Pubs

--- publish/tests_pub_docs.py
+++ publish/tests_pub_docs.py
@@ -13,7 +13,7 @@ class PubDocTest(DjangoTest):
         self.assertFiles('Documents', 500, 2600)
 
     def test_doc_directories(self):
-        data = '''Documents/SHRINKING-WORLD-PUBS,700,1025'''
+        data = '''Documents/SHRINKING-WORLD-PUBS,1048,1055'''
         for x in list(reader(data.splitlines())):
             # print(x)
             if x[2:]:

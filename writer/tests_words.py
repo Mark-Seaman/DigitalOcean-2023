d5d831e16a5a48066dbd6c8a5489c4b475113ce9
Mark Seaman <seaman@SeaMac>
2023-10-20 08:41:14 -0600

    Fix tests

--- writer/tests_words.py
+++ writer/tests_words.py
@@ -15,11 +15,11 @@ class WordCountTest(DjangoTest):
     def test_words_in_content_nodes(self):
         text = measure_pub_words()
         # print(len(text_lines(text)), 'Lines of text in word files')
-        self.assertEqual(len(text_lines(text)), 1722)
+        self.assertEqual(len(text_lines(text)), 1726)
 
     def test_content_nodes(self):
         pubs, contents, words, pages = count_nodes()
         self.assertEqual(pubs, 19)
-        self.assertEqual(contents, 1310)
-        self.assertEqual(words, 469021)
-        self.assertEqual(pages, 1876)
+        self.assertEqual(contents, 1314)
+        self.assertEqual(words, 470281)
+        self.assertEqual(pages, 1881)

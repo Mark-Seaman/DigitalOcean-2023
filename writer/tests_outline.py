73a288e37db48dc3f747a430046d85784ba67471
macbook <seaman@macbook>
2023-11-17 09:21:47 -0700

    To do

--- writer/tests_outline.py
+++ writer/tests_outline.py
@@ -8,25 +8,22 @@ from .pub_script import pub_path
 class OutlineTest(DjangoTest):
 
     def test_outline_urls(self):
-        path = pub_path('spirituality','Transformation','Index.md')
+        path = pub_path('spirituality', 'Transformation', 'Index.md')
         x = extract_urls(path)
         self.assertEqual(len(x), 6)
 
     def test_outline_links(self):
-        path = pub_path('spirituality','Transformation','Index.md')
+        path = pub_path('spirituality', 'Transformation', 'Index.md')
         x = extract_links(path)
         self.assertEqual(len(x), 5)
 
     def test_create_index(self):
-        path = pub_path('spirituality','Worship')
+        path = pub_path('spirituality', 'Worship')
         create_index(path)
-        
+
     def test_outlines(self):
-        path = pub_path('spirituality','Worship')
-        create_outlines(path)
-        
+        path = pub_path('spirituality', 'Worship')
+        print(create_outlines(path))
+
         # x = '\n'.join([x for x in x])
         # self.assertEqual(len(x), 1047)
-
-    
-

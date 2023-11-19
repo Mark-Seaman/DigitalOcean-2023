2b86f7b0e1f8e2eb4f0cefb5254360e3d1dead6a
Mark Seaman <seaman@SeaMac>
2023-11-19 15:57:02 -0700

    Remove JSON files

--- probe/probe_json.py
+++ probe/probe_json.py
@@ -12,23 +12,27 @@ def test_data_json():
     status = 'Data JSON:\n'
 
     # Pubs
-    build_pubs()
-    text = save_json_data('config/publish.json', 'publish')
-    status += f'    config/publish.json  {len(text)} characters\n'
+    if is_old('config/publish.json'):
+        build_pubs()
+        text = save_json_data('config/publish.json', 'publish')
+        status += f'    config/publish.json  {len(text)} characters\n'
 
     # Courses
-    import_all_courses()
-    s = workspace_path(course='bacs350', project='_students.csv')
-    import_students(s)
-    text = save_json_data('config/course.json')
-    status += f'    config/course.json  {len(text)} characters\n'
+    if is_old('config/course.json'):
+        import_all_courses()
+        s = workspace_path(course='bacs350', project='_students.csv')
+        import_students(s)
+        text = save_json_data('config/course.json')
+        status += f'    config/course.json  {len(text)} characters\n'
 
     # Tasks
-    text = save_json_data('config/task.json', 'task')
-    status += f'    config/task.json    {len(text)} characters\n'
+    if is_old('config/task.json'):
+        text = save_json_data('config/task.json', 'task')
+        status += f'    config/task.json    {len(text)} characters\n'
 
     # Data
-    text = save_json_data('config/data.json')
-    status += f'    config/data.json    {len(text)} characters\n'
+    if is_old('config/data.json'):
+        text = save_json_data('config/data.json')
+        status += f'    config/data.json    {len(text)} characters\n'
 
     return status
